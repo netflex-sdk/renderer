8c97236a6c3e5e5918e4fb287f72e6aeea07a20c
Thomas Alrek <thomas@alrek.no>
2021-05-20 18:39:52 +0200

    Improve error handling

--- src/Exceptions/RenderException.php
+++ src/Exceptions/RenderException.php
@@ -23,10 +23,21 @@ class RenderException extends Exception implements ProvidesSolution
     public function __construct(ResponseInterface $response)
     {
         $message = $this->parseResponse($response);
-        $error = json_decode('{' . Str::beforeLast(Str::after($message, '{'), '}') . '}');
 
-        if (json_last_error() === JSON_ERROR_NONE && isset($error->message)) {
-            $message = $error->message;
+        if (!is_object($message)) {
+            $error = json_decode('{' . Str::beforeLast(Str::after($message, '{'), '}') . '}');
+
+            if (json_last_error() === JSON_ERROR_NONE && isset($error->message)) {
+                $message = $error->message;
+            }
+        } else {
+            if (property_exists($message, 'message')) {
+                $message = $message->message;
+            }
+        }
+
+        if (!is_string($message)) {
+            $message = json_encode($message, JSON_PRETTY_PRINT);
         }
 
         parent::__construct($message);
